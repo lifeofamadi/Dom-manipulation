cb75849755b7a4ad84ef49fede0f5503cc21ff3d
lifeofamadi <amadivictor3310@gmail.com>
2023-02-08 20:17:43 +0100

    Dom manipulation

---
+++ appp.js
@@ -0,0 +1,14 @@
+
+const reavealBtn = document.querySelector('.reveal-btn');
+
+const hiddenContent = document.querySelector('.hidden-content');
+
+function revealContent(){
+    if(hiddenContent.classList.contains('reveal-btn')){
+        hiddenContent.classList.remove('reveal-btn')
+    }else{
+        hiddenContent.classList.add('reveal-btn')
+    }
+}
+
+reavealBtn.addEventListener('click', revealContent)
